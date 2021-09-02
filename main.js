e97be0698f6389ab8eca4456fedb8050cc0578b1
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-09-02 17:59:39 +0100

    JS added, minor CSS updates.

--- main.js
+++ main.js
@@ -1,1 +1,89 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', getCountriesApi);
+
+// The main container where the countries will be added. ----------------
+let mainCountriesCont = document.querySelector('.countriesContainer');
+
+function getCountriesApi(){
+
+  fetch("https://restcountries.eu/rest/v2/all")
+  .then(res => {
+    if (!res.ok) {
+      throw Error('error')
+    } else {
+      return res.json()
+      }
+    }
+  )
+  .then(data => {
+    let countries = data.map(country => {
+      let countryObject = {
+        name: country.name,
+        capital: country.capital,
+        population: country.population,
+        region: country.region,
+        subRegion: country.subregion,
+        flag: country.flag
+      }
+    return countryObject;
+    })
+    console.log(`A country object example:`, countries[0]);
+    country(countries);
+  })
+  .catch(error => console.log(error));
+}
+
+
+function country(array){
+  console.log(`The item is an Array: ${Array.isArray(array)}`);
+  console.log(`The number of countries is: ${array.length}`);
+    for (let i = 0; i < array.length; i++){
+      // Creating an individual country div.
+      let countryDiv = document.createElement("div");
+      countryDiv.classList.add("countryContainer");
+      // Creating the country info div.
+      let countryInfoDiv = document.createElement("div");
+      countryInfoDiv.classList.add("countryInfo");
+      // Creating the separate country info points.
+      // Name. --------------------------------------
+      let nameParagraph = document.createElement("p");
+      nameParagraph.classList.add("highlight");
+      let nameText = document.createTextNode(`${array[i].name}`);
+      nameParagraph.appendChild(nameText);
+      countryInfoDiv.appendChild(nameParagraph);
+      // Capital. --------------------------------------
+      let capitalParagraph = document.createElement("p");
+      let capitalText = document.createTextNode(`Capital: ${array[i].capital}`);
+      capitalParagraph.appendChild(capitalText);
+      countryInfoDiv.appendChild(capitalParagraph);
+      // Population. --------------------------------------
+      let populationParagraph = document.createElement("p");
+      let populationText = document.createTextNode(`Pop: ${array[i].population}`);
+      populationParagraph.appendChild(populationText);
+      countryInfoDiv.appendChild(populationParagraph);
+      // Region. --------------------------------------
+      let regionParagraph = document.createElement("p");
+      let regionText = document.createTextNode(`Region: ${array[i].region}`);
+      regionParagraph.appendChild(regionText);
+      countryInfoDiv.appendChild(regionParagraph);
+      // Sub-Region. --------------------------------------
+      let subRegionParagraph = document.createElement("p");
+      let subRegionText = document.createTextNode(`Sub-Reg: ${array[i].subRegion}`);
+      subRegionParagraph.appendChild(subRegionText);
+      countryInfoDiv.appendChild(subRegionParagraph);
+      // Creating the country info div and img elements.
+      let countryFlagDiv = document.createElement("div");
+      countryFlagDiv.classList.add("countryFlag");
+      let flagImg = document.createElement("img");
+      flagImg.setAttribute("src", `${array[i].flag}`);
+      flagImg.setAttribute("alt", `The flag of ${array[i].name}`);
+      countryFlagDiv.appendChild(flagImg);
+
+
+      countryDiv.appendChild(countryInfoDiv);
+      countryDiv.appendChild(countryFlagDiv);
+
+      mainCountriesCont.appendChild(countryDiv);
+  }
+}
